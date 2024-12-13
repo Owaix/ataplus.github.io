19e2fb4ed31564da3dff449b02781c18e0be4c60
TL\owaitmuh <muhammad.owais@rowriter.com>
2024-12-13 20:10:20 +0500

    sas

--- src/app/service/env.ts
+++ src/app/service/env.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://localhost:5000/api/'
+  // BASE_URL: 'http://localhost:5000/api/'
+  BASE_URL: 'http://ataplus-staging.ap-southeast-1.elasticbeanstalk.com/api/'
 }
