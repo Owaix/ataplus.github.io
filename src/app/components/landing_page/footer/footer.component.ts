1ace3f49aecb5c14e5a1cd834f8b9d96b894ea22
TL\owaitmuh <muhammad.owais@rowriter.com>
2024-12-23 20:39:39 +0500

    dsd

--- src/app/components/landing_page/footer/footer.component.ts
+++ src/app/components/landing_page/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, AfterViewInit } from "@angular/core";
+import { Component, OnDestroy, AfterViewInit, HostListener } from "@angular/core";
 
 @Component({
   selector: 'footer',
@@ -7,21 +7,31 @@ import { Component, OnDestroy, AfterViewInit } from "@angular/core";
 })
 export class Footer implements AfterViewInit, OnDestroy {
   nav = ['FAQ', 'Terms And Condition', 'Privacy Policy', 'Contact Us', 'Blog'];
-
+  sections = {
+    aboutUs: false,
+    getStarted: false,
+    legal: false
+  };
   nav2 = [];
+  isMobile = false;
 
   ngAfterViewInit(): void {
-    // const svgs = document.getElementsByClassName('icons-social')[0].getElementsByTagName('svg');
+  }
 
-    // for (let i = 0; i < svgs.length; i++) {
-    //   svgs[i].addEventListener('mouseover', () => {
-    //     svgs[i].getElementsByTagName('path')[0].setAttribute('fill', 'var(--lime-green)');
-    //   });
+  @HostListener('window:resize', [])
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 767;
+    if (!this.isMobile) {
+      this.sections = {
+        aboutUs: false,
+        getStarted: false,
+        legal: false
+      };
+    }
+  }
 
-    //   svgs[i].addEventListener('mouseout', () => {
-    //     svgs[i].getElementsByTagName('path')[0].setAttribute('fill', '#FFF');
-    //   });
-    // }
+  toggleSection(section: keyof typeof this.sections) {
+    this.sections[section] = !this.sections[section];
   }
 
   ngOnDestroy(): void {
