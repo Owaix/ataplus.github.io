09ee9c3b87d3652340758cb94214d870baf05e76
TL\owaitmuh <muhammad.owais@rowriter.com>
2025-01-17 20:07:18 +0500

    dsds

--- src/app/components/landing_page/main/article/article.component.ts
+++ src/app/components/landing_page/main/article/article.component.ts
@@ -34,10 +34,10 @@ export class ArticleItem implements AfterViewInit {
   private isInViewport(element: HTMLElement) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
 }
